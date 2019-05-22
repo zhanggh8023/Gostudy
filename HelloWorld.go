7715f1b572354f86a36630d08e9f1c619bdee439
zhanggh <zhanggh@citydo.com.cn>
2019-05-22 10:52:39 +0800

    start study

---
+++ HelloWorld.go
@@ -0,0 +1,20 @@
+/**
+* -*- encoding: utf-8 -*-
+*v@Time    : 2019/5/22 9:50
+* @Author  : zgh
+* @Email   : 849080458@qq.com
+* @Software: GoLand
+* @File    : HelloWorld.go
+**/
+
+package main
+
+import "fmt"
+
+//func main() {
+//	fmt.Println("Hello, World!")
+//}
+
+func main() {
+	fmt.Println("This is first go!")
+}
